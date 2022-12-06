d0f7ecff7fb53c63f52f64626899f02c248a1690
ignusxd12 <87685215+IgnacioRezet@users.noreply.github.com>
2022-12-06 12:09:17 -0400

    Prueba tecnica
    Se sube prueba tecnica ibm reac+.net

---
+++ Pruebas_Tecnica_IBM/Test-Backend/Test-Backend/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Test_Backend.Entidades;
+
+namespace Test_Backend.Controllers
+{
+   
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        static HttpClient client = new HttpClient();
+        [HttpGet]
+        [Route("User")]
+        public async Task<IActionResult> User()
+        {
+            List<User> lstUser = new List<User>();
+            HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+            if (response.IsSuccessStatusCode)
+            {                
+                var json = await response.Content.ReadAsStringAsync();              
+                lstUser = JsonConvert.DeserializeObject<List<User>>(json);
+               
+            }
+            return StatusCode(StatusCodes.Status200OK, lstUser);
+        }
+    }
+}
---
+++ Pruebas_Tecnica_IBM/Test-Backend/Test-Backend/Program.cs
@@ -0,0 +1,40 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+//No olvidar habilitar Cors en caso si consumo esta api desde React.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000", "https://localhost:7135/api/user/User").AllowAnyHeader()
+                 .AllowAnyMethod(); 
+                      });
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
